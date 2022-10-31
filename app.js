b622cdbffb980e59064208392fbb90d7a5eef8ee
Vũ Hoàng Đức <47014053+nghiate142@users.noreply.github.com>
2022-10-31 15:15:45 +0700

    summary

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+import express from "express";
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+// const ejs = require("ejs");
+const port = 3000;
 const app = express();
-const express = require("express");
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// mongodb import
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const connectMongoose = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("connect to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.listen(port, () => {
+  connectMongoose();
+  console.log("hello world");
+});
