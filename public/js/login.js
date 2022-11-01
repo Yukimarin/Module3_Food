0b2e916b90481ae9334d0be38459664c962dc7c2
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2022-11-02 04:06:18 +0700

    Summary

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,13 @@
 let dk = document.getElementById("btn-dk");
+let submit = document.getElementById("btn-submit");
+// dieu huong register
 dk.addEventListener("click", (e) => {
   console.log("hahah");
   window.location.href = "http://localhost:3000/register";
+});
+
+// dieu huong home
+submit.addEventListener("click", (e) => {
+  console.log("haha");
+  window.location.href = "http://localhost:3000/";
 });
