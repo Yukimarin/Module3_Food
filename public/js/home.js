e8599b6f96e8c442bd15f976ef9e772061ebf9bc
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2022-11-02 09:34:48 +0700

    Summary

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,13 @@
 let login = document.getElementById("btn-login");
-login.addEventListener("click", (e) => {
-  //   console.log("aaaaa");
-  window.location.href = "http://localhost:3000/login";
+let info = document.getElementById("info");
+let edit = document.getElementById("edit-user");
+
+// login.addEventListener("click", (e) => {
+//   //   console.log("aaaaa");
+//   window.location.href = "http://localhost:3000/login";
+// });
+
+info.addEventListener("click", (e) => {
+  console.log("haha");
+  edit.style.display = "block";
 });
